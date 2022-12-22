95f8ac778ac57957d0a07f56d838050972b784cc
brnooliveira <brno.oliveiras@gmail.com>
2022-12-22 20:53:20 -0300

    test

---
+++ core/management/commands/wait_for_db.py
@@ -0,0 +1,8 @@
+"""
+Django command for wait for the database to be available.
+"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
